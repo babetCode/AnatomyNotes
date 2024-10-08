ca7ccfacf756bba8bc8105d83f69c79558ab4758
babetCode <adrienbabet1@gmail.com>
2024-10-08 14:25:55 -0600

    .

--- categorization.py
+++ categorization.py
@@ -1,12 +1,19 @@
 # Anatomical Categories
 
-systems = ['skeletal', 'muscular', 'nervous', 'cardiovascular', 'respiratory', 'digestive', 'endocrine', 'reproductive', 'integumentary']
+# ordinal suffices
+def ordinal(n: int) -> str:
+    return f"{n}{'th' if 11 <= n % 100 <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th')}"
 
-skeletal = []
+def lr(lst: list) -> list:
+    bilateral = []
+    for item in lst:
+        bilateral.append(item+' (right)')
+        bilateral.append(item+' (left)')
+    return bilateral
 
-axial = []
+# systems = ['skeletal', 'muscular', 'nervous', 'cardiovascular', 'respiratory', 'digestive', 'endocrine', 'reproductive', 'integumentary']
 
-# vertebrae
+# trunk
 cervical = ['c'+str(i+1) for i in range(7)]
 thoracic = ['t'+str(i+1) for i in range(12)]
 lumbar = ['l'+str(i+1) for i in range(5)]
@@ -16,18 +23,24 @@ for region in vertebral_regions:
     for vertebra in region:
         vertebrae.append(vertebra)
 vertebrae.append('sacrum')
+vertebrae.append('coccyx')
 
-# ordinal suffices
-def ordinal(n: int) -> str:
-    return f"{n}{'th' if 11 <= n % 100 <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th')}"
-
-# ribs
 ribs = [ordinal(n+1)+str(' rib') for n in range(12)]
 
-# pelvis
 pelvis = ['ilium', 'ischeum', 'pubis']
 
-#skull
-cranial = ['frontal', 'parietal']
+print(lr(pelvis))
 
+sternum = ['sternum (manubrium)', 'sternum (body)', 'sternum (xiphoid process)']
+
+#skull
+cranial = ['frontal', 'parietal', 'temporal', 'occipital']
+facial = ['maxilla', 'mandible', 'zygomatic', 'lacrimal', 'nasal', 'turbinate', 'vomer', 'palatine']
+ear = ['incus', 'malleus', 'stapes']
+skull_regions = [cranial, facial, ear]
+skull = []
+for region in skull_regions:
+    for bone in region:
+        skull.append(bone)
+skull.append('hyoid')
 
